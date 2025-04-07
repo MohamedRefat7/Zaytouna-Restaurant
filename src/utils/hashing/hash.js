3ee4f25d1be42304d661b831f426792837e1f8a8
MostafaNegm222 <mostafanegm0002@gmail.com>
2025-04-07 05:07:33 +0200

    edit pagination

--- src/utils/hashing/hash.js
+++ src/utils/hashing/hash.js
@@ -7,5 +7,9 @@ export const hash = ({ plainText, saltRounds = process.env.SALT }) => {
 
 // compare
 export const compareHash = ({ plainText, hash }) => {
+  if (!plainText || !hash) {
+    console.error("❌ Missing plainText or hash:", { plainText, hash });
+    return false;
+  }
   return bcrypt.compareSync(plainText, hash);
 };
